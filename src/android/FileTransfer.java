e8f22ed9080679201fce60c8d3d75f889a08dd60
enimax-anime <107899019+enimax-anime@users.noreply.github.com>
2024-12-23 22:04:32 -0800

    Update FileTransfer.java

--- src/android/FileTransfer.java
+++ src/android/FileTransfer.java
@@ -45,7 +45,6 @@ import org.apache.cordova.CordovaResourceApi.OpenForReadResult;
 import org.apache.cordova.LOG;
 import org.apache.cordova.PluginManager;
 import org.apache.cordova.PluginResult;
-import org.apache.cordova.Whitelist;
 import org.apache.cordova.file.FileUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -683,31 +682,7 @@ public class FileTransfer extends CordovaPlugin {
          * return a Whitelist object. Since the fixed whitelist is removed in Cordova 4.x,
          * the correct call now is to shouldAllowRequest from the plugin manager.
          */
-        Boolean shouldAllowRequest = null;
-        if (isLocalTransfer) {
-            shouldAllowRequest = true;
-        }
-        if (shouldAllowRequest == null) {
-            try {
-                Method gwl = webView.getClass().getMethod("getWhitelist");
-                Whitelist whitelist = (Whitelist)gwl.invoke(webView);
-                shouldAllowRequest = whitelist.isUrlWhiteListed(source);
-            } catch (NoSuchMethodException e) {
-            } catch (IllegalAccessException e) {
-            } catch (InvocationTargetException e) {
-            }
-        }
-        if (shouldAllowRequest == null) {
-            try {
-                Method gpm = webView.getClass().getMethod("getPluginManager");
-                PluginManager pm = (PluginManager)gpm.invoke(webView);
-                Method san = pm.getClass().getMethod("shouldAllowRequest", String.class);
-                shouldAllowRequest = (Boolean)san.invoke(pm, source);
-            } catch (NoSuchMethodException e) {
-            } catch (IllegalAccessException e) {
-            } catch (InvocationTargetException e) {
-            }
-        }
+        Boolean shouldAllowRequest = true;
 
         if (!Boolean.TRUE.equals(shouldAllowRequest)) {
             LOG.w(LOG_TAG, "Source URL is not in white list: '" + source + "'");
